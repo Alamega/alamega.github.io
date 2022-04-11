27ed7fa10ae508a0e499bc9e30a876bf24f125c5
Alamega <vladshuman@gmail.com>
2022-04-11 22:14:04 +0300

    fix hello box

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,7 +1,7 @@
 function hello() {
     let input = document.getElementById('input_hello');
     let result = document.getElementById('result_hello');
-    if (input.value.length > 20) {
+    if (input.value.length > 30) {
         result.innerHTML = "Ахахаха как смешно (нет)";
     } else {
         result.innerHTML = "Здравствуйте, " + input.value + " !";
