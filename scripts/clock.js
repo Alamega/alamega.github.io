ee003432b255b6b689cd21abb5df7099f49b9f79
Alamega <vladshuman@gmail.com>
2022-04-15 20:06:48 +0300

    fix flexbox items

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,13 +1,17 @@
+var date;  
+var time;
+var clockv = document.getElementById('clock');;
+
 function clock()
 {
-  var date = new Date();  
-  var time = [date.getHours(),date.getMinutes(),date.getSeconds()];
+  date = new Date();  
+  time = [date.getHours(),date.getMinutes(),date.getSeconds(),date.getMilliseconds()];
   if(time[0] < 10){time[0] = "0"+ time[0];}
   if(time[1] < 10){time[1] = "0"+ time[1];}
   if(time[2] < 10){time[2] = "0"+ time[2];}
-  var clock = document.getElementById('clock');
-  clock.innerHTML = [time[0],time[1],time[2]].join(':');
-  setInterval("clock()", 1000);
+  if(time[3] < 10){time[3] = "00"+ time[3];}
+  if(time[3] < 100 && time[3] >= 10){time[3] = "0"+ time[3];}
+  clockv.innerHTML = [time[0],time[1],time[2]].join(':')+ " "+ time[3];
 }  
 
-clock();
+setInterval("clock()", 1);
