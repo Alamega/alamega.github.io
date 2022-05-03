b8508b1089ec698b6ce5dbd63aa3bdb809d54b9f
Alamega <vladshuman@gmail.com>
2022-05-03 20:38:36 +0300

    add Enc to RSA

--- scripts/RSA.js
+++ scripts/RSA.js
@@ -1,12 +1,21 @@
+var p;
+var q;
+var d;
+var mod;
+var F;
+var e;
+var arr = [ null, null, 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М',
+'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э','Ю', 'Я'
+];
+console.log(arr.indexOf('Ш'));
 function GetKeys () {
-    let p = document.getElementById('p').value;
-    let q = document.getElementById('q').value;
-    let d = document.getElementById('d').value;
+    p = document.getElementById('p').value;
+    q = document.getElementById('q').value;
+    d = document.getElementById('d').value;
     let result = document.getElementById('result');
     if (!(p.length>5 || q.length>5 || d.length>5)) {
-        let mod = p*q;
-        let F = (p-1)*(q-1);
-        let e;
+        mod = p*q;
+        F = (p-1)*(q-1);
         for (i = 0; i < F; i++) {
             if ((d*i)%F==1){
                 e = i;
@@ -17,4 +26,37 @@ function GetKeys () {
     } else {
         result.innerHTML = "Значения p, q, Kc не должны быть больше 99999";
     }
+}
+
+function Enc () {
+    function EncChar (x) {
+        return Math.pow(arr.indexOf(x),e)%mod;
+    }
+    let encStr = document.getElementById('encStr').value;
+    let resultStr = "";
+    for (let i = 0; i < encStr.length; i++) {
+        resultStr+=EncChar(encStr[i])+" ";
+    }
+    let result = document.getElementById('result_enc');
+    result.innerHTML = "Зашифрованная строка: " + resultStr;
+}
+
+function Dec () {
+    function DecInt (x) {
+        return Math.pow(x,d)%mod;
+    }
+    console.log("DecInt(27) = "+DecInt(27));
+    console.log("Говнина(рыганина) = " + Math.pow(27,23)%91);
+    let decStr = document.getElementById('decStr').value;
+    arrDec = decStr.split(' ');
+    //arrDec Работает
+    let resultStr = "";
+    for (let i = 0; i < arrDec.length; i++) {
+        console.log("i = "+arrDec[i]);
+        console.log("d = "+DecInt(arrDec[i]));
+        
+        resultStr+=arr[DecInt(arrDec[i])];
+    }
+    let result = document.getElementById('result_dec');
+    result.innerHTML = "Расшифрованная строка: " + resultStr;
 }
