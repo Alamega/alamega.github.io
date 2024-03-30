b4ea84734108eab536826c2b4a83aeaec643e902
Alamega <vladshuman@gmail.com>
2024-03-30 23:00:07 +0300

    вот теперь так будет

--- services.js
+++ services.js
@@ -1,18 +1,23 @@
 const services = [
-  { desc: "Java Spring Boot REST API", link: "https://alamega-api.onrender.com" },
-  { desc: "Next JS", link: "https://alamega.onrender.com" },
+  {
+    desc: "Java Spring Boot REST API",
+    link: "https://alamega-api.onrender.com",
+    img: "/images/spring.svg",
+  },
+  {
+    desc: "Next JS",
+    link: "https://alamega.onrender.com",
+    img: "/images/next.svg",
+  },
 ];
 
 services.forEach(async function (service) {
   const link = document.createElement("a");
-  link.href = service.link;
   document.getElementById("services").appendChild(link);
-  link.innerHTML = service.desc + `<span style="color: red"> (запускается...)<span/>`;
-  fetch(service.link, { mode: "no-cors" })
-    .then((response) => {
-      link.innerHTML = service.desc + `<span style="color: green"> (работает)<span/>`;
-    })
-    .catch((error) => {
-      link.innerHTML = service.desc + " (работает)";
-    });
+  link.href = service.link;
+  link.innerHTML = `<img src="` + service.img + `""/>` + service.desc + "(запускается...)";
+  link.style.color = "red";
+  await fetch(service.link, { mode: "no-cors" });
+  link.innerHTML = `<img src="` + service.img + `""/>` + service.desc + " (работает)";
+  link.style.color = "green";
 });
